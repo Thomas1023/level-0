9d07492842dbb278b7a42721699dfeb7068636c8
Goldeenzu <league@WTS10.attlocal.net>
2016-09-18 13:01:42 -0700

    4

---
+++ src/zork.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class zork {
+ public static void main(String[] args) {
+	 Scanner kb=new Scanner(System.in);
+	 System.out.println("korz use comm to gett all commands");
+	 String ans1  = kb.next();
+	 
+	 if(ans1.equals("comm")){
+		 System.out.println("eat"); 
+	 }
+	 }
+}
+
